12986a3a4ca1c87a3c8c03f8ad65d81f6e8939b5
akshaypandey28 <akshaychaman2004@gmail.com>
2026-06-07 13:19:11 +0530

    implemented some more routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,23 @@
 import express from "express";
-import {createTweet} from '../../controllers/tweet-controller.js';
+import {createTweet,getTweet} from '../../controllers/tweet-controller.js';
 import {toggleLike} from '../../controllers/like-controller.js';
+import {createComment} from '../../controllers/comment-controller.js';
+import {createUser} from '../../controllers/user-controller.js';
 
 const router = express.Router();
 
+//routes related to tweets
 router.post('/tweets', createTweet);
+router.get('/tweets/:id',getTweet);
 
-
+//routes related to likes
 router.post('/likes/toggle',toggleLike);
+
+//routes related to comments
+router.post('/comments', createComment);
+
+
+//routes related to users
+router.post('/users', createUser);
 
 export default router;
